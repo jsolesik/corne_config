124b22fae399d283510c70cdfd0fedd3c566285b
Jaroslaw S. Olesik <jaroslaw.olesik@gmal.com>
2025-12-14 04:23:27 +0100

    fixes

--- jso/jso_behaviors.h
+++ jso/jso_behaviors.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define HR(MOD, TAP) &b_hr MOD TAP
-#define MT(MOD, TAP) &b_mt MOD TAP
-#define LT(LAYER, TAP) &b_lt LAYER TAP
+#define B_MT(MOD, TAP) &b_mt MOD TAP
+#define B_LT(LAYER, TAP) &b_lt LAYER TAP
 #define MT_SLASH &slash_lt
 #define MT_DOT &dot_gt
 #define MT_COLON &colon_qmark
